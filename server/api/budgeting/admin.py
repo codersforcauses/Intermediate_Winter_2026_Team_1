827f83e2fb901c18f4a972412a9b983de3979fe5
Siyu Zhu <lilyzhusiyu@gmail.com>
2026-07-11 15:08:10 +0800

    changed the Budget name to SavingGoal; added fields to SavingGoal; added another model DepositWithdrawal to track the amount changes of saving goals

--- server/api/budgeting/admin.py
+++ server/api/budgeting/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from .models import SavingGoal, DepositWithdrawal
 
 # Register your models here.
-admin.site.register("Budget")
+admin.site.register(SavingGoal)
+admin.site.register(DepositWithdrawal)
--- server/api/budgeting/models.py
+++ server/api/budgeting/models.py
@@ -1,15 +1,25 @@
 from django.db import models
 
 # Create your models here.
-class Budget(models.Model):
+class SavingGoal(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     target_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    aim_date = models.DateField()
+    target_date = models.DateField()
     saving_frequency = models.CharField(max_length=20, choices=[('daily', 'Daily'), ('weekly', 'Weekly'), ('monthly', 'Monthly')])
     created_at = models.DateTimeField(auto_now_add=True)
+    deposits_withdrawals = models.JSONField(default=list, blank=True)  # Store deposits and withdrawals as a list of dictionaries
+    status = models.CharField(max_length=20, choices=[('active', 'Active'), ('completed', 'Completed'), ('paused', 'Paused')], default='active')
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
-    
+
+class DepositWithdrawal(models.Model):
+    saving_goal = models.ForeignKey(SavingGoal, on_delete=models.CASCADE, related_name='transactions')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_type = models.CharField(max_length=10, choices=[('deposit', 'Deposit'), ('withdrawal', 'Withdrawal')])
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.transaction_type.capitalize()} of {self.amount} for {self.saving_goal.name}"
