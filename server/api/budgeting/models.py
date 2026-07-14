bc8ecc6273aa64f94ec527c9b3868c4ddef2ad26
Siyu Zhu <lilyzhusiyu@gmail.com>
2026-07-14 12:50:13 +0800

    Link the SavingGoal model to User; comment out he deposits_withdrawals field as it is duplicated to the DepositWithdrawal model

--- server/api/budgeting/models.py
+++ server/api/budgeting/models.py
@@ -1,14 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class SavingGoal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='saving_goals')
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     target_amount = models.DecimalField(max_digits=10, decimal_places=2)
     target_date = models.DateField()
     saving_frequency = models.CharField(max_length=20, choices=[('daily', 'Daily'), ('weekly', 'Weekly'), ('monthly', 'Monthly')])
     created_at = models.DateTimeField(auto_now_add=True)
-    deposits_withdrawals = models.JSONField(default=list, blank=True)  # Store deposits and withdrawals as a list of dictionaries
+    #deposits_withdrawals = models.JSONField(default=list, blank=True)  # Store deposits and withdrawals as a list of dictionaries
     status = models.CharField(max_length=20, choices=[('active', 'Active'), ('completed', 'Completed'), ('paused', 'Paused')], default='active')
     updated_at = models.DateTimeField(auto_now=True)
 
