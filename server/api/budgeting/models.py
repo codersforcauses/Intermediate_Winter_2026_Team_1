1fc40ec57246968f0c73150de44926120cf09f7d
Siyu Zhu <lilyzhusiyu@gmail.com>
2026-07-14 13:19:40 +0800

    modify the budget model link to user to allow self defined user later; added a saving_amount field to SavingGoal model

--- server/api/budgeting/models.py
+++ server/api/budgeting/models.py
@@ -1,21 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+
+user = models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    on_delete=models.CASCADE,
+    related_name="saving_goals",
+)
 
 # Create your models here.
 class SavingGoal(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='saving_goals')
-    name = models.CharField(max_length=100)
+    purpose = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     target_amount = models.DecimalField(max_digits=10, decimal_places=2)
     target_date = models.DateField()
     saving_frequency = models.CharField(max_length=20, choices=[('daily', 'Daily'), ('weekly', 'Weekly'), ('monthly', 'Monthly')])
     created_at = models.DateTimeField(auto_now_add=True)
+    saving_amount = models.DecimalField(max_digits=10,decimal_places=2,)
     #deposits_withdrawals = models.JSONField(default=list, blank=True)  # Store deposits and withdrawals as a list of dictionaries
     status = models.CharField(max_length=20, choices=[('active', 'Active'), ('completed', 'Completed'), ('paused', 'Paused')], default='active')
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return self.purpose
 
 class DepositWithdrawal(models.Model):
     saving_goal = models.ForeignKey(SavingGoal, on_delete=models.CASCADE, related_name='transactions')
@@ -24,4 +32,4 @@ class DepositWithdrawal(models.Model):
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.transaction_type.capitalize()} of {self.amount} for {self.saving_goal.name}"
+        return f"{self.transaction_type.capitalize()} of {self.amount} for {self.saving_goal.purpose}"
